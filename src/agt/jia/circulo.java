ab4b60034016a94a86f2ce4ac772486cd012ca5d
Luan Vinicius <luan.vini31@hotmail.com>
2022-02-25 22:44:32 -0300

    comentários das alterações

---
+++ src/agt/jia/circulo.java
@@ -0,0 +1,22 @@
+// Internal action code for project teste
+
+package jia;
+
+import jason.*;
+import jason.asSemantics.*;
+import jason.asSyntax.*;
+
+public class circulo extends DefaultInternalAction {
+
+    @Override
+    public Object execute(TransitionSystem ts, Unifier un, Term[] args) throws Exception {
+        // execute the internal action
+        ts.getAg().getLogger().info("executing internal action 'jia.circulo'");
+        if (true) { // just to show how to throw another kind of exception
+            throw new JasonException("not implemented!");
+        }
+
+        // everything ok, so returns true
+        return true;
+    }
+}
